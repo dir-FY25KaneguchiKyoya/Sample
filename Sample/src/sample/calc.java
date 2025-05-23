6faee7185d5cf6225a02512fef5463f271b35163
dir-FY25KaneguchiKyoya <kyoya.kaneguchi.e057@dirbato.co.jp>
2025-05-23 14:44:46 +0900

    chapter5

--- Sample/src/sample/calc.java
+++ Sample/src/sample/calc.java
@@ -1,10 +1,31 @@
 package sample;
 
 public class calc {
-
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        var num1 = 22;
+        var num2 = 11;
 
+        // 足し算
+        var result1 = num1 + num2;
+        System.out.println("num1 + num2 = " + result1);
+
+        // 引き算
+        var result2 = num1 - num2;
+        System.out.println("num1 - num2 = " + result2);
+
+        // 掛け算
+        var result3 = num1 * num2;
+        System.out.println("num1 * num2 = " + result3);
+
+        // 割り算
+        var result4 = num1 / num2;
+        System.out.println("num1 / num2 = " + result4);
+
+        // 剰余
+        var result5 = num1 % num2;
+        System.out.println("num1 % num2 = " + result5);
+        
+        // 文字列の連結
+        System.out.println("num1 + num2 = " + result1);
     }
-
 }
