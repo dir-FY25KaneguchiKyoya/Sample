b6cecef1936f6bb75c2b46d39097adc8444158d4
dir-FY25KaneguchiKyoya <kyoya.kaneguchi.e057@dirbato.co.jp>
2025-05-23 13:02:34 +0900

    13:02

---
+++ Sample/src/sample/Test2.java
@@ -0,0 +1,9 @@
+public class Sample {
+
+    public static void main(String[] args) {
+        String if = "何らかのデータ";
+        if = "別の何か";
+
+        System.out.println(if);
+    }
+}
--- Sample/src/sample/Variable.java
+++ Sample/src/sample/Variable.java
@@ -3,8 +3,12 @@ package sample;
 public class Variable {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        var number1 = 1;
+        System.out.println(number1 * 2);
 
+        String str1;
+        str1 = "abc";
+        System.out.println(str1);
     }
 
 }
